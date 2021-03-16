38c99dcbd8ba703ce2450debe8377d4bbc79bc48
Zach Orris <zlorris@gmail.com>
2021-03-16 17:41:04 -0400

    Added Dark Mode + Profile Links
    Dark Mode button is located in the top right corner of the page.  Profile Links are the icons to the left of the Dark Mode button.

---
+++ javascripts/script.js
@@ -0,0 +1,42 @@
+window.onload = function() {
+	// set the height of main tag using screen size
+	const height = screen.availHeight;
+  const collect = document.getElementsByTagName("main");
+
+  collect[0].style.minHeight = height/2 + "px";
+
+  displayMode();
+}
+
+const updateMode = function () {
+	if (localStorage.getItem("theme") === "dark")
+		localStorage.setItem("theme", "light");
+	else
+		localStorage.setItem("theme", "dark");
+
+	displayMode();
+}
+
+const displayMode = function() {
+
+	if (localStorage.getItem("theme") === "dark") {
+		document.getElementsByTagName("h1")[0].classList.add("header-dark");
+		document.getElementsByTagName("body")[0].classList.add("body-dark");
+		document.getElementsByTagName("main")[0].classList.add("main-dark");
+
+		document.getElementById("sunmoon").src = "images/sun.png";
+		document.getElementById("githubpic").src = "images/github_dark.png";
+		document.getElementById("linkedinpic").src = "images/linkedin_dark.png";
+		document.getElementById("hspic").src = "images/handshake_dark.png";
+	} else {
+		document.getElementsByTagName("h1")[0].classList.remove("header-dark");
+		document.getElementsByTagName("body")[0].classList.remove("body-dark");
+		document.getElementsByTagName("main")[0].classList.remove("main-dark");
+
+		document.getElementById("sunmoon").src = "images/moon.png";
+		document.getElementById("githubpic").src = "images/github.png";
+		document.getElementById("linkedinpic").src = "images/linkedin.png";
+		document.getElementById("hspic").src = "images/handshake.png";
+	}
+}
+
